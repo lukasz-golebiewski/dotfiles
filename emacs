08ba6727d0b46a2dde5caf3da88e93c37b826046
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2018-06-08 19:09:03 +0200

    Remove double-line util

--- emacs
+++ emacs
@@ -175,7 +175,6 @@
 (key-chord-mode 1)
 (key-chord-define-global "sw" 'ace-swap-window)
 (key-chord-define-global "df" 'helm-do-ag-project-root)
-(key-chord-define-global "cd" 'double-line)
 (key-chord-define-global "r[" 'replace-regexp)
 (key-chord-define-global "r]" 'projectile-replace-regexp)
 
--- emacs.d/config/init-utils.el
+++ emacs.d/config/init-utils.el
@@ -21,35 +21,6 @@
   (comment-or-uncomment-region (line-beginning-position) (line-end-position))
   (next-line))
 
-(defun copy-line (arg)
-    "Copy lines (as many as prefix argument) in the kill ring.
-      Ease of use features:
-      - Move to start of next line.
-      - Appends the copy on sequential calls.
-      - Use newline as last char even on the last line of the buffer.
-      - If region is active, copy its lines."
-    (interactive "p")
-    (let ((beg (line-beginning-position))
-          (end (line-end-position arg)))
-      (when mark-active
-        (if (> (point) (mark))
-            (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
-          (setq end (save-excursion (goto-char (mark)) (line-end-position)))))
-      (if (eq last-command 'copy-line)
-          (kill-append (buffer-substring beg end) (< end beg))
-        (kill-ring-save beg end)))
-    (kill-append "\n" nil)
-    (beginning-of-line (or (and arg (1+ arg)) 2))
-    (if (and arg (not (= 1 arg))) (message "%d lines copied" arg)))
-
-
-(defun double-line (arg)
-  "copy line and place it below the original"
-  (interactive "p")
-  (copy-line arg)
-  (yank)
-  (move-end-of-line))
-
 (defun toggle-window-split ()
   (interactive)
   (if (= (count-windows) 2)
