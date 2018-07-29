38745fb7e9bf7b02d58b1b56075a2b55b074e637
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2018-07-30 00:03:26 +0200

    Use intero for haskell

--- emacs.d/config/init-haskell.el
+++ emacs.d/config/init-haskell.el
@@ -1,13 +1,5 @@
 ;; haskell
+
 (use-package haskell-mode)
-
-(setq haskell-mode-hook (quote (turn-on-haskell-indentation)))
-
-(eval-after-load "haskell-mode"
-    '(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile))
-(eval-after-load "haskell-cabal"
-    '(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-compile))
-(setq haskell-compile-cabal-build-command "stack build")
-(defun my-haskell-mode-hook ()
-  (local-set-key (kbd "C-c C-r") 'inferior-haskell-load-and-run))
-(add-hook 'haskell-mode-hook 'my-haskell-mode-hook)
+(use-package intero)
+(add-hook 'haskell-mode-hook 'intero-mode)
