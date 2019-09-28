e19569ebdd557a86c3b48ea50396442d945c3cc5
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-09-28 19:18:19 +0200

    Use dante instead of intero

--- emacs.d/config/init-haskell.el
+++ emacs.d/config/init-haskell.el
@@ -3,10 +3,22 @@
 (use-package haskell-mode
   :pin melpa
 )
-(use-package intero
-  :pin melpa
-)
-(add-hook 'haskell-mode-hook 'intero-mode)
-(add-hook 'intero-mode-hook
-          (lambda() (local-set-key (kbd "C-c l") 'intero-goto-definition))
-)
+
+;;(use-package intero
+;;  :pin melpa
+;;)
+;;(add-hook 'haskell-mode-hook 'intero-mode)
+;;(add-hook 'intero-mode-hook
+;;          (lambda() (local-set-key (kbd "C-c l") 'intero-goto-definition))
+;;)
+
+(use-package dante
+  :ensure t
+  :after haskell-mode
+  :commands 'dante-mode
+  :init
+  (add-hook 'haskell-mode-hook 'flycheck-mode)
+  ;; OR:
+  ;; (add-hook 'haskell-mode-hook 'flymake-mode)
+  (add-hook 'haskell-mode-hook 'dante-mode)
+  )
