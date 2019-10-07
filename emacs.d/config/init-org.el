02e4df8a9d55d57211023cd6a6453241d32e21ac
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-10-07 18:35:33 +0200

    Remap org-store-link

--- emacs.d/config/init-org.el
+++ emacs.d/config/init-org.el
@@ -1,6 +1,6 @@
 (use-package org)
 
-(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cs" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-iswitchb)
