65c3b261be35cf357e3e7ed253a8225683bdba3c
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-10-16 19:11:46 +0200

    Back to intero

--- emacs.d/config/init-haskell.el
+++ emacs.d/config/init-haskell.el
@@ -4,21 +4,10 @@
   :pin melpa
 )
 
-;;(use-package intero
-;;  :pin melpa
-;;)
-;;(add-hook 'haskell-mode-hook 'intero-mode)
-;;(add-hook 'intero-mode-hook
-;;          (lambda() (local-set-key (kbd "C-c l") 'intero-goto-definition))
-;;)
-
-(use-package dante
-  :ensure t
-  :after haskell-mode
-  :commands 'dante-mode
-  :init
-  (add-hook 'haskell-mode-hook 'flycheck-mode)
-  ;; OR:
-  ;; (add-hook 'haskell-mode-hook 'flymake-mode)
-  (add-hook 'haskell-mode-hook 'dante-mode)
-  )
+(use-package intero
+  :pin melpa
+)
+(add-hook 'haskell-mode-hook 'intero-mode)
+(add-hook 'intero-mode-hook
+          (lambda() (local-set-key (kbd "C-c .") 'intero-goto-definition))
+)
