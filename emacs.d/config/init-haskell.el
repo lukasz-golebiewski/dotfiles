86ecd5243ee89a239e6ac5cd065a5752356e8d5e
Lukasz Golebiewski <lukasz.golebiewski@gmail.com>
2019-11-04 21:56:07 +0100

    [haskell] use dante

--- emacs.d/config/init-haskell.el
+++ emacs.d/config/init-haskell.el
@@ -4,10 +4,23 @@
   :pin melpa
 )
 
-(use-package intero
-  :pin melpa
-)
-(add-hook 'haskell-mode-hook 'intero-mode)
-(add-hook 'intero-mode-hook
-          (lambda() (local-set-key (kbd "C-c .") 'intero-goto-definition))
-)
+(use-package flycheck-haskell)
+(add-hook 'haskell-mode-hook #'flycheck-haskell-setup)
+
+(use-package dante
+  :ensure t
+  :after haskell-mode
+  :commands 'dante-mode
+  :init
+  (add-hook 'haskell-mode-hook 'flycheck-mode)
+  ;; OR:
+  ;; (add-hook 'haskell-mode-hook 'flymake-mode)
+  (add-hook 'haskell-mode-hook 'dante-mode)
+  )
+
+(add-hook 'dante-mode-hook
+   '(lambda () (flycheck-add-next-checker 'haskell-dante
+                '(warning . haskell-hlint))))
+
+(provide 'init-haskell)
+;;; init-haskell.el ends here
