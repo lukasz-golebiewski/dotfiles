6cbe9e0d561be0e3504bed4f9766b257a8789cb9
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-11-24 12:11:18 +0100

    Remove dante

--- emacs.d/config/init-haskell.el
+++ emacs.d/config/init-haskell.el
@@ -7,20 +7,5 @@
 (use-package flycheck-haskell)
 (add-hook 'haskell-mode-hook #'flycheck-haskell-setup)
 
-(use-package dante
-  :ensure t
-  :after haskell-mode
-  :commands 'dante-mode
-  :init
-  (add-hook 'haskell-mode-hook 'flycheck-mode)
-  ;; OR:
-  ;; (add-hook 'haskell-mode-hook 'flymake-mode)
-  (add-hook 'haskell-mode-hook 'dante-mode)
-  )
-
-(add-hook 'dante-mode-hook
-   '(lambda () (flycheck-add-next-checker 'haskell-dante
-                '(warning . haskell-hlint))))
-
 (provide 'init-haskell)
 ;;; init-haskell.el ends here
--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -7,70 +7,11 @@
  '(custom-safe-themes
    (quote
     ("6c62b1cd715d26eb5aa53843ed9a54fc2b0d7c5e0f5118d4efafa13d7715c56e" "38ba6a938d67a452aeb1dada9d7cdeca4d9f18114e9fc8ed2b972573138d4664" "0fb6369323495c40b31820ec59167ac4c40773c3b952c264dd8651a3b704f6b5" "196cc00960232cfc7e74f4e95a94a5977cb16fd28ba7282195338f68c84058ec" "51e228ffd6c4fff9b5168b31d5927c27734e82ec61f414970fc6bcce23bc140d" "c9e123d4ecd9ceb056806c6297336763e9e96eed6962bfc1d5252afcc2761610" default)))
- '(dante-methods-alist
-   (quote
-    ((styx "styx.yaml"
-           ("styx" "repl" dante-target))
-     (new-impure-nix dante-cabal-new-nix
-                     ("nix-shell" "--run"
-                      (concat "cabal new-repl "
-                              (or dante-target
-                                  (dante-package-name)
-                                  "")
-                              " --builddir=dist-repl/dante")))
-     (new-nix dante-cabal-new-nix
-              ("nix-shell" "--pure" "--run"
-               (concat "cabal new-repl "
-                       (or dante-target
-                           (dante-package-name)
-                           "")
-                       " --builddir=dist-repl/dante")))
-     (nix dante-cabal-nix
-          ("nix-shell" "--pure" "--run"
-           (concat "cabal repl "
-                   (or dante-target "")
-                   " --builddir=dist-repl/dante")))
-     (impure-nix dante-cabal-nix
-                 ("nix-shell" "--run"
-                  (concat "cabal repl "
-                          (or dante-target "")
-                          " --builddir=dist-repl/dante")))
-     (new-build "cabal.project.local"
-                ("cabal" "new-repl"
-                 (or dante-target
-                     (dante-package-name)
-                     nil)
-                 "--builddir=dist-repl/dante"))
-     (nix-ghci
-      #[257 "\300\301\302#\207"
-            [directory-files t "shell.nix\\|default.nix"]
-            5 "
-
-(fn D)"]
-      ("nix-shell" "--pure" "--run" "ghci"))
-     (stack "stack.yaml"
-            ("stack" "repl" dante-target))
-     (mafia "mafia"
-            ("mafia" "repl" dante-target))
-     (bare-cabal
-      #[257 "\300\301\302#\207"
-            [directory-files t "..cabal$"]
-            5 "
-
-(fn D)"]
-      ("cabal" "repl" dante-target "--builddir=dist-repl/dante"))
-     (bare-ghci
-      #[257 "\300\207"
-            [t]
-            2 "
-
-(fn _)"]
-      ("ghci")))))
  '(git-gutter:update-interval 2)
  '(org-agenda-files (quote ("~/notes/DOING.org")))
  '(package-selected-packages
    (quote
-    (lsp-mode sbt-mode haskell-mode evil nix-mode dante zoom-window yasnippet yaml-mode which-key vlf use-package suggest smex smartparens scala-mode nyan-mode multiple-cursors monokai-theme lsp-ui lsp-scala lsp-haskell lcr key-chord idris-mode hydra helm-swoop helm-projectile helm-lsp helm-etags-plus helm-ag git-timemachine git-gutter flycheck-haskell evil-surround evil-mc evil-magit evil-leader eno elmacro dockerfile-mode csv-mode company-lsp ag ace-window)))
+    (lsp-mode sbt-mode haskell-mode evil nix-mode zoom-window yasnippet yaml-mode which-key vlf use-package suggest smex smartparens scala-mode nyan-mode multiple-cursors monokai-theme lsp-ui lsp-scala lsp-haskell lcr key-chord idris-mode hydra helm-swoop helm-projectile helm-lsp helm-etags-plus helm-ag git-timemachine git-gutter flycheck-haskell evil-surround evil-mc evil-magit evil-leader eno elmacro dockerfile-mode csv-mode company-lsp ag ace-window)))
  '(safe-local-variable-values (quote ((python-shell-interpreter . "python3"))))
  '(zoom-window-mode-line-color "DarkGreen"))
 
