b58a600f82bfa5ba9aabc499bfeaeeb78a46e942
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-11-24 12:12:44 +0100

    Make lint happy in custom.el

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -1,3 +1,8 @@
+;;; custom.el -- Emacs custom variables
+
+;;; Commentary:
+
+;;; Code:
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -21,3 +26,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(provide 'custom)
+;;; custom.el ends here
