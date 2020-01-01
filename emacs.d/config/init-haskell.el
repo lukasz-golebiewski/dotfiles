f9940b7424fbb8a1315618138f59d91a36ca8197
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2020-01-01 18:46:50 +0100

    Use ghcide

--- emacs.d/config/init-haskell.el
+++ emacs.d/config/init-haskell.el
@@ -7,6 +7,29 @@
   :pin melpa
 )
 
+;; LSP
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode t))
+(use-package yasnippet
+  :ensure t)
+(use-package lsp-mode
+  :ensure t
+  :hook (haskell-mode . lsp)
+  :commands lsp)
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+(use-package lsp-haskell
+ :ensure t
+ :config
+ (setq lsp-haskell-process-path-hie "ghcide")
+ (setq lsp-haskell-process-args-hie '())
+ ;; Comment/uncomment this line to see interactions between lsp client/server.
+ ;;(setq lsp-log-io t)
+)
+
 (use-package flycheck-haskell)
 (add-hook 'haskell-mode-hook #'flycheck-haskell-setup)
 
