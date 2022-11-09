1de9607e477134895bf1e2c6d2a34871eefc745f
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2022-11-09 12:40:34 +0100

    Re-work init-haskell

--- emacs.d/config/init-haskell.el
+++ emacs.d/config/init-haskell.el
@@ -3,21 +3,33 @@
 ;;; Commentary:
 
 ;;; Code:
-(use-package haskell-mode
-  :init (setq haskell-stylish-on-save t)
-  :pin melpa
-)
-
-;;(use-package flycheck-haskell)
-;;(add-hook 'haskell-mode-hook #'flycheck-haskell-setup)
-
-(use-package lsp-haskell
-  :pin melpa
-)
-
+;; if you want to change prefix for lsp-mode keybindings.
 (use-package lsp-mode
-    :hook (haskell-mode . lsp-deferred)
-    :commands (lsp lsp-deferred))
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (haskell-mode . lsp-deferred)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp-deferred)
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+;; if you are helm user
+(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+
+;; optionally if you want to use debugger
+;; (use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; optional if you want which-key integration
+(use-package which-key
+    :config
+    (which-key-mode))
+
+;; (use-package flycheck-haskell)
+;; (add-hook 'haskell-mode-hook #'flycheck-haskell-setup)
 
 (provide 'init-haskell)
 ;;; init-haskell.el ends here
